3fadddb2c353459bffbce7b6ca7041a21cc94e87
danieldebassige <danieldebassige@gmail.com>
2022-03-27 13:48:38 -0500

    Make darkmode persistent
    Added localstorage variable to make the dark mode selection persistent between reloads

--- javascript.js
+++ javascript.js
@@ -2,15 +2,32 @@ function swapStyleSheet(sheet) {
     document.getElementById("stylesheet").setAttribute("href", sheet);
 }
 
+function getStyle() {
+    style = localStorage.getItem('style')
+
+    if(style == null) {
+        localStorage.setItem('style', "light")
+    }
+    
+    if(style == "dark") {
+        swapStyleSheet("foundation-dark.css")
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var checkbox = document.getElementById("darkMode");
+    if(localStorage.getItem('style') == "dark") {
+        checkbox.checked = true;
+    }
 
     checkbox.addEventListener('change', function() {
         if(checkbox.checked) {
             swapStyleSheet("foundation-dark.css")
+            localStorage.setItem('style', "dark")
         }
         else {
             swapStyleSheet("foundation-light.css")
+            localStorage.setItem('style', "light")
         }
     })
 })
